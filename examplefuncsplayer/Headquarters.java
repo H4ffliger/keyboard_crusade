4e60b3e2bf0be8d25494b3ccd95674f5e1873f0f
Markus Schlienger <schlienger30@gmail.com>
2023-01-10 23:12:30 +0100

    Initiate adding of HQ Locations to Array

--- examplefuncsplayer/Headquarters.java
+++ examplefuncsplayer/Headquarters.java
@@ -2,12 +2,14 @@ package examplefuncsplayer;
 
 import battlecode.common.*;
 
-import static examplefuncsplayer.RobotPlayer.directions;
-import static examplefuncsplayer.RobotPlayer.rng;
+import static examplefuncsplayer.RobotPlayer.*;
 
 public class Headquarters {
 
     static void runHeadquarters(RobotController rc) throws GameActionException {
+        if(turnCount==0) {
+            initiateArray(rc);
+        }
         // Pick a direction to build in.
         Direction dir = directions[rng.nextInt(directions.length)];
         MapLocation newLoc = rc.getLocation().add(dir);
@@ -29,5 +31,16 @@ public class Headquarters {
                 rc.buildRobot(RobotType.LAUNCHER, newLoc);
             }
         }
+    }
+
+    private static void initiateArray(RobotController rc) throws GameActionException{
+        for (int i=0;i<4;i++) {
+            if(rc.readSharedArray(i)==0) {
+                String ownInformationString = rc.getLocation().toString();
+                int ownInformation = Integer.parseInt(ownInformationString);
+                rc.writeSharedArray(i,ownInformation);
+            }
+        }
+
     }
 }
