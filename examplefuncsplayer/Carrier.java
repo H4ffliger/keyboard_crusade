5412a2bc64b0c2f3b7b38ee833416d08c0c1820e
Markus Schlienger <schlienger30@gmail.com>
2023-01-11 00:33:48 +0100

    Initiate adding of HQ Locations to Array Plus move carrier when full to nearest HQ

--- examplefuncsplayer/Carrier.java
+++ examplefuncsplayer/Carrier.java
@@ -2,16 +2,19 @@ package examplefuncsplayer;
 
 import battlecode.common.*;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
-import static examplefuncsplayer.RobotPlayer.directions;
-import static examplefuncsplayer.RobotPlayer.rng;
+import static examplefuncsplayer.RobotPlayer.*;
 
 public class Carrier {
 
+    static ArrayList<MapLocation> hqLocations = new ArrayList<>();
+
     static void runCarrier(RobotController rc) throws GameActionException {
+        if (turnCount < 2) {
+            rc.setIndicatorString("Initiate HQ");
+            initiateHQ(rc);
+        }
         if (rc.getAnchor() != null) {
             // If I have an anchor singularly focus on getting it to the first island I see
             int[] islands = rc.senseNearbyIslands();
@@ -35,44 +38,89 @@ public class Carrier {
                 }
             }
         }
-        // Try to gather from squares around us.
+
         MapLocation me = rc.getLocation();
+
+        // If we can see a well, move towards it
+        WellInfo[] wells = rc.senseNearbyWells();
+        if (wells.length > 1) {
+            WellInfo well_one = wells[1];
+            Direction dir = me.directionTo(well_one.getMapLocation());
+            if (rc.canMove(dir))
+                rc.move(dir);
+        }
+
+        // Try to gather from squares around us.
         for (int dx = -1; dx <= 1; dx++) {
             for (int dy = -1; dy <= 1; dy++) {
                 MapLocation wellLocation = new MapLocation(me.x + dx, me.y + dy);
                 if (rc.canCollectResource(wellLocation, -1)) {
-                    if (rng.nextBoolean()) {
-                        rc.collectResource(wellLocation, -1);
-                        rc.setIndicatorString("Collecting, now have, AD:" +
-                                rc.getResourceAmount(ResourceType.ADAMANTIUM) +
-                                " MN: " + rc.getResourceAmount(ResourceType.MANA) +
-                                " EX: " + rc.getResourceAmount(ResourceType.ELIXIR));
-                    }
+                    rc.collectResource(wellLocation, -1);
+                    rc.setIndicatorString("Collecting, now have, AD:" +
+                            rc.getResourceAmount(ResourceType.ADAMANTIUM) +
+                            " MN: " + rc.getResourceAmount(ResourceType.MANA) +
+                            " EX: " + rc.getResourceAmount(ResourceType.ELIXIR));
                 }
             }
         }
+
+        //If full, go to the nearest HQ
+        if (30 < rc.getResourceAmount(ResourceType.ADAMANTIUM) + rc.getResourceAmount(ResourceType.MANA) + rc.getResourceAmount(ResourceType.ELIXIR)) {
+            if (hqLocations != null) {
+                int minimalDistance = 1000;
+                MapLocation bestHQ = null;
+                for (MapLocation hq : hqLocations) {
+                    int actualDistance = me.distanceSquaredTo(hq);
+                    if (actualDistance < minimalDistance) {
+                        minimalDistance = actualDistance;
+                        bestHQ = hq;
+                    }
+                }
+                rc.setIndicatorString("HQ: " + Objects.requireNonNull(bestHQ));
+                if (rc.canSenseRobotAtLocation(bestHQ)) {
+                    rc.transferResource(bestHQ, ResourceType.ADAMANTIUM, rc.getResourceAmount(ResourceType.ADAMANTIUM));
+                    rc.transferResource(bestHQ, ResourceType.MANA, rc.getResourceAmount(ResourceType.MANA));
+                    rc.transferResource(bestHQ, ResourceType.ELIXIR, rc.getResourceAmount(ResourceType.ELIXIR));
+                } else {
+                    Direction dir = me.directionTo(bestHQ);
+                    if (rc.canMove(dir)) {
+                        rc.move(dir);
+                    }
+                }
+            } else {
+                rc.setIndicatorString("HQ is null!");
+            }
+        }
+
+
         // Occasionally try out the carriers attack
-        if (rng.nextInt(20) == 1) {
+        /*if (rng.nextInt(20) == 1) {
             RobotInfo[] enemyRobots = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
             if (enemyRobots.length > 0) {
                 if (rc.canAttack(enemyRobots[0].location)) {
                     rc.attack(enemyRobots[0].location);
                 }
             }
-        }
+        }*/
 
-        // If we can see a well, move towards it
-        WellInfo[] wells = rc.senseNearbyWells();
-        if (wells.length > 1 && rng.nextInt(3) == 1) {
-            WellInfo well_one = wells[1];
-            Direction dir = me.directionTo(well_one.getMapLocation());
-            if (rc.canMove(dir))
-                rc.move(dir);
-        }
+
         // Also try to move randomly.
         Direction dir = directions[rng.nextInt(directions.length)];
         if (rc.canMove(dir)) {
             rc.move(dir);
+        }
+    }
+
+    private static void initiateHQ(RobotController rc) throws GameActionException {
+        for (int i = 0; i < 4; i++) {
+            int hqLocation = rc.readSharedArray(i);
+            String hqLocationString = Integer.toString(hqLocation);
+            String xString = hqLocationString.substring(1, 3);
+            String yString = hqLocationString.substring(3, 5);
+            int dx = Integer.parseInt(xString);
+            int dy = Integer.parseInt(yString);
+            hqLocations.add(new MapLocation(dx, dy));
+            rc.setIndicatorString("Set HQ to " + hqLocationString);
         }
     }
 }
--- examplefuncsplayer/Headquarters.java
+++ examplefuncsplayer/Headquarters.java
@@ -7,7 +7,7 @@ import static examplefuncsplayer.RobotPlayer.*;
 public class Headquarters {
 
     static void runHeadquarters(RobotController rc) throws GameActionException {
-        if(turnCount==0) {
+        if(turnCount<2) {
             initiateArray(rc);
         }
         // Pick a direction to build in.
@@ -16,17 +16,17 @@ public class Headquarters {
         if (rc.canBuildAnchor(Anchor.STANDARD)) {
             // If we can build an anchor do it!
             rc.buildAnchor(Anchor.STANDARD);
-            rc.setIndicatorString("Building anchor! " + rc.getAnchor());
+            //rc.setIndicatorString("Building anchor! " + rc.getAnchor());
         }
         if (rng.nextBoolean()) {
             // Let's try to build a carrier.
-            rc.setIndicatorString("Trying to build a carrier");
+            //rc.setIndicatorString("Trying to build a carrier");
             if (rc.canBuildRobot(RobotType.CARRIER, newLoc)) {
                 rc.buildRobot(RobotType.CARRIER, newLoc);
             }
         } else {
             // Let's try to build a launcher.
-            rc.setIndicatorString("Trying to build a launcher");
+            //rc.setIndicatorString("Trying to build a launcher");
             if (rc.canBuildRobot(RobotType.LAUNCHER, newLoc)) {
                 rc.buildRobot(RobotType.LAUNCHER, newLoc);
             }
@@ -36,9 +36,23 @@ public class Headquarters {
     private static void initiateArray(RobotController rc) throws GameActionException{
         for (int i=0;i<4;i++) {
             if(rc.readSharedArray(i)==0) {
-                String ownInformationString = rc.getLocation().toString();
-                int ownInformation = Integer.parseInt(ownInformationString);
+                int x = rc.getLocation().x;
+                int y = rc.getLocation().y;
+                String xString,yString;
+                if (x<10) {
+                    xString = "0" + x;
+                } else {
+                    xString = Integer.toString(x);
+                }
+                if (y<10) {
+                    yString = "0" + y;
+                } else {
+                    yString = Integer.toString(y);
+                }
+                String ownInformationIntegerString = 1 + xString + yString;
+                int ownInformation = Integer.parseInt(ownInformationIntegerString);
                 rc.writeSharedArray(i,ownInformation);
+                rc.setIndicatorString("HQ is here: " + ownInformationIntegerString);
             }
         }
 
