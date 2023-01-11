23cf95d56f05c46cff53c23effdf92139001dc44
Markus Schlienger <schlienger30@gmail.com>
2023-01-12 00:31:07 +0100

    Carrier goes to Well and brings resource back

--- examplefuncsplayer/Carrier.java
+++ examplefuncsplayer/Carrier.java
@@ -9,12 +9,14 @@ import static examplefuncsplayer.RobotPlayer.*;
 public class Carrier {
 
     static ArrayList<MapLocation> hqLocations = new ArrayList<>();
-    static int goal = 1;
+
+    static ArrayList<MapLocation> adWells = new ArrayList<>();
+    static ArrayList<MapLocation> manaWells = new ArrayList<>();
+    static int goal = 1, globalIndex = 0;
 
     static void runCarrier(RobotController rc) throws GameActionException {
-        if (turnCount == 1) {
-            initiateHQ(rc);
-        }
+
+        getImportantLocations(rc);
         if (rc.getAnchor() != null) {
             // If I have an anchor singularly focus on getting it to the first island I see
             int[] islands = rc.senseNearbyIslands();
@@ -39,24 +41,78 @@ public class Carrier {
             }
         }
         MapLocation me = rc.getLocation();
-        if(goal==1) {
-            // If we can see a well, move towards it
-            WellInfo[] wells = rc.senseNearbyWells();
-            if (wells.length > 1) {
-                WellInfo well_one = wells[1];
-                Direction dir = me.directionTo(well_one.getMapLocation());
-                if (rc.canMove(dir))
-                    rc.move(dir);
-            } else {
-                // Also try to move randomly.
-                Direction dir = directions[rng.nextInt(directions.length)];
-                if (rc.canMove(dir)) {
-                    rc.move(dir);
+        if (goal == 1) {
+            WellInfo[] nearbyWells = rc.senseNearbyWells();
+            if (nearbyWells.length > 0) {
+                MapLocation nearestWell = nearbyWells[0].getMapLocation();
+                if (me.isAdjacentTo(nearestWell) || me.distanceSquaredTo(nearestWell) == 1) {
+                    System.out.println("Can act on Well: ");
+                    if (rc.canCollectResource(nearestWell, -1)) {
+                        rc.collectResource(nearestWell, -1);
+                        rc.setIndicatorString("Collecting, now have, AD:" +
+                                rc.getResourceAmount(ResourceType.ADAMANTIUM) +
+                                " MN: " + rc.getResourceAmount(ResourceType.MANA) +
+                                " EX: " + rc.getResourceAmount(ResourceType.ELIXIR));
+                    }
+                } else {
+                    Direction dir = me.directionTo(nearestWell);
+                    if (rc.canMove(dir)) {
+                        rc.setIndicatorString("Move to nearest Well");
+                        rc.move(dir);
+                    }
                 }
+
+            } else if (adWells.size() != 0 || manaWells.size() != 0) {
+                int minimalDistance = 100000;
+                MapLocation nearestWell = null;
+                for (MapLocation adWell : adWells) {
+                    int actualDistance = me.distanceSquaredTo(adWell);
+                    if (actualDistance < minimalDistance) {
+                        minimalDistance = actualDistance;
+                        nearestWell = adWell;
+                    }
+                }
+                for (MapLocation manaWell : manaWells) {
+                    int actualDistance = me.distanceSquaredTo(manaWell);
+                    if (actualDistance < minimalDistance) {
+                        minimalDistance = actualDistance;
+                        nearestWell = manaWell;
+                    }
+                }
+                if (nearestWell != null) {
+                    if (me.isAdjacentTo(nearestWell) || me.distanceSquaredTo(nearestWell) == 1) {
+                        if (rc.canCollectResource(nearestWell, -1)) {
+                            rc.collectResource(nearestWell, -1);
+                            rc.setIndicatorString("Collecting, now have, AD:" +
+                                    rc.getResourceAmount(ResourceType.ADAMANTIUM) +
+                                    " MN: " + rc.getResourceAmount(ResourceType.MANA) +
+                                    " EX: " + rc.getResourceAmount(ResourceType.ELIXIR));
+                        }
+                    } else {
+                        Direction dir = me.directionTo(nearestWell);
+                        if (rc.canMove(dir)) {
+                            rc.setIndicatorString("Move to nearest Well");
+                            rc.move(dir);
+                        }
+                    }
+                } else {
+                    System.out.println("NearestWell is null");
+                }
+
+            } else {
+                rc.setIndicatorString("Wells are null!");
+            }
+
+
+            // Also try to move randomly.
+            Direction dir = directions[rng.nextInt(directions.length)];
+            if (rc.canMove(dir)) {
+                rc.move(dir);
+
             }
 
             // Try to gather from squares around us.
-            for (int dx = -1; dx <= 1; dx++) {
+            /*for (int dx = -1; dx <= 1; dx++) {
                 for (int dy = -1; dy <= 1; dy++) {
                     MapLocation wellLocation = new MapLocation(me.x + dx, me.y + dy);
                     if (rc.canCollectResource(wellLocation, -1)) {
@@ -67,12 +123,12 @@ public class Carrier {
                                 " EX: " + rc.getResourceAmount(ResourceType.ELIXIR));
                     }
                 }
-            }
+            }*/
             if (rc.getResourceAmount(ResourceType.ADAMANTIUM) + rc.getResourceAmount(ResourceType.MANA) + rc.getResourceAmount(ResourceType.ELIXIR) == 40) {
-                goal=2;
+                goal = 2;
                 System.out.println("Goal = 2");
             }
-        } else if(goal == 2) {
+        } else if (goal == 2) {
             if (hqLocations != null) {
                 int minimalDistance = 100000;
                 MapLocation bestHQ = null;
@@ -115,7 +171,7 @@ public class Carrier {
                 rc.setIndicatorString("HQ are null!");
             }
             if (rc.getResourceAmount(ResourceType.ADAMANTIUM) + rc.getResourceAmount(ResourceType.MANA) + rc.getResourceAmount(ResourceType.ELIXIR) == 0) {
-                goal=1;
+                goal = 1;
                 System.out.println("Goal = 1");
             }
         } else {
@@ -126,7 +182,6 @@ public class Carrier {
             }
             System.out.println("Goal = something else");
         }
-
 
 
         // Occasionally try out the carriers attack
@@ -140,21 +195,27 @@ public class Carrier {
         }*/
 
 
-
     }
 
-    private static void initiateHQ(RobotController rc) throws GameActionException {
-        int i = 0;
-        while (rc.readSharedArray(i) != 0) {
-            int hqLocation = rc.readSharedArray(i);
-            String hqLocationString = Integer.toString(hqLocation);
-            String xString = hqLocationString.substring(1, 3);
-            String yString = hqLocationString.substring(3, 5);
+    private static void getImportantLocations(RobotController rc) throws GameActionException {
+        while (rc.readSharedArray(globalIndex) != 0) {
+            int information = rc.readSharedArray(globalIndex);
+            String informationString = Integer.toString(information);
+            String firstBitString = informationString.substring(0, 1);
+            String xString = informationString.substring(1, 3);
+            String yString = informationString.substring(3, 5);
+            int firstBit = Integer.parseInt(firstBitString);
             int dx = Integer.parseInt(xString);
             int dy = Integer.parseInt(yString);
-            hqLocations.add(new MapLocation(dx, dy));
+            if (firstBit == 1) {
+                hqLocations.add(new MapLocation(dx, dy));
+            } else if (firstBit == 2) {
+                adWells.add(new MapLocation(dx, dy));
+            } else if (firstBit == 3) {
+                manaWells.add(new MapLocation(dx, dy));
+            }
             //System.out.println("Set HQ to " + hqLocationString);
-            i++;
+            globalIndex++;
         }
     }
 }
