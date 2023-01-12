d5c19687a7b9770689267d1414ba3ebbf0c37c59
JoshuaHaefliger_KeynetAG <joshua@4getit.net>
2023-01-12 17:00:12 +0100

    Launchers will now explore in the first 200 rounds

--- examplefuncsplayer/Launcher.java
+++ examplefuncsplayer/Launcher.java
@@ -1,19 +1,32 @@
 package examplefuncsplayer;
 
 import battlecode.common.*;
+import scala.Int;
 
 import java.util.ArrayList;
 
-import static examplefuncsplayer.Pathfinding.returnToHomeBase;
+import static examplefuncsplayer.Pathfinding.*;
+import static examplefuncsplayer.RobotPlayer.rng;
 
 public class Launcher {
 
     //static int radioArray;
     static ArrayList<MapLocation> hqLocations = new ArrayList<>();
+    static Integer exploreID;
+    int eOffsetX;
+    int eOffsetY;
+
+    //Amount of rounds until the agent changes the explore offset again.
+    int exploreSoftness = 10;
+
     static void runLauncher(RobotController rc) throws GameActionException {
 
-
-        explore();
+        //Creation process
+        if(exploreID == null){
+            //ToDo: Change and coordinate with HQ shared array
+            exploreID = rc.getRoundNum()%9;
+            //System.out.println("Rnd: " + Integer.toString(exploreID));
+        }
 
         //Set the local hq Positions
         for (int i = 0; i < 4; i++) {
@@ -43,10 +56,19 @@ public class Launcher {
 
 
         //Move with the pathfinding module
+
+        //ToDo: Temporary exploring function for testing
         try {
-            rc.move(returnToHomeBase(rc, hqLocations.get(0).x, hqLocations.get(0).y));
+            if (rc.getRoundNum() < 200) {
+                explore(rc, exploreID);
+            } else {
+                rc.move(returnToHomeBase(rc, hqLocations.get(0).x, hqLocations.get(0).y));
+            }
         }
-        catch (Exception ignore){}
+        catch (Exception e){
+            rc.setIndicatorString("Error: " + e.toString());
+        }
+
 
 
     }
@@ -54,19 +76,22 @@ public class Launcher {
     /*
     mapFieldID for example 9 or 16 for the amount of subfields of the map
      */
-    static void explore(RobotController rc, int mapFieldID){
+    static void explore(RobotController rc, int mapFieldID) throws GameActionException {
         int mapH = rc.getMapHeight();
         int mapW = rc.getMapWidth();
+        int mapSize = mapH / 6;
 
-        int mapSize = mapH /20;
-        int tX, tY;
+        //if(rc.getRoundNum()/);
 
-        for (int z = 9-1; z >= 0; z--){
-            tX = z % mapSize;
-            tY = z / mapSize;
-            System.out.println("Explore map Size: " + Integer.toString(mapSize));
+        int tX = mapFieldID / 3 * mapW / 3 + mapSize;
+        int tY = mapFieldID % 3 * mapH / 3 + mapSize;
+       //System.out.println("mapFieldID: " + Integer.toString(mapFieldID) + " | tX: " + Integer.toString(tX) + " | tY" + Integer.toString(tY));
+
+        try {
+            rc.move(goToPosition(rc, tX, tY));
+        }
+        catch (Exception ignore){}
+
         }
 
     }
-
-}
